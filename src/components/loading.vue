f6bad7adf8148b41f61ecbe655a72dee02506d39
= <=>
2024-09-27 16:45:31 +0900

    9.27 마지막

--- src/components/loading.vue
+++ src/components/loading.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-if="isLoading" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-    <div class="text-white text-lg mt-4"><img src="../assets/spinner2.svg" alt="Loading"></div>
+    <div class="text-white text-lg mt-4"><img src="../assets/spinner1.gif" alt="Loading"></div>
   </div>
   </template>
   
@@ -14,7 +14,7 @@ const store = useStore() ;
 const isLoading = ref(store.state.loading);
   // 컴포넌트가 마운트될 때 로딩 상태 변경
  watch (() => store.state.loading , (newValue, oldValue) => {
-    console.log("오긴오나");
+    
     isLoading.value = newValue ;
  })
   </script>
